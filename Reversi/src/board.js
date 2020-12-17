baaeb3ebcc2f41f5ecd8e7cdcc38f483f915c2c6
jane baik <janebaik@berkeley.edu>
2020-12-17 10:35:51 -0800

    get piece

--- Reversi/src/board.js
+++ Reversi/src/board.js
@@ -53,8 +53,12 @@ Board.prototype.isValidPos = function (pos) {     // [0, 1]
  * throwing an Error if the position is invalid.
  */
 Board.prototype.getPiece = function (pos) {
-};
-
+  if (this.isValidPos(pos)){
+    return this.grid[pos[0]][pos[1]];
+  } else {
+    throw new Error("Not valid pos!")
+  };
+}
 /**
  * Checks if the piece at a given position
  * matches a given color.
